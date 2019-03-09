ffaf5dc8876d55f7dc74bf61cae661805831001e
Kyle Fricilone <kfricilone@gmail.com>
2019-03-09 02:29:49 -0600

    Add initial shield manager/generator

---
+++ UwURevenge/Assets/ShieldArr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldArr : MonoBehaviour
+{
+   public List<GameObject> shields = new List<GameObject>();
+
+   public void OnDeath(GameObject obj)
+   {
+      shields.Remove(obj);
+      if (shields.Count < 1)
+      {
+         //Disable overall shield
+      }
+   }
+   
+}
