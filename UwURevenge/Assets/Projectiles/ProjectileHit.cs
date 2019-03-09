6c258fa7516e9e98bb31753388f6f042d3f45c1d
Laban <laban.colyott@gmail.com>
2019-03-08 23:19:20 -0600

    Projectiles, autocannon, test-enemies, effects, post-processing

---
+++ UwURevenge/Assets/Projectiles/ProjectileHit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ProjectileHit : MonoBehaviour {
+
+	public float damage;
+	public GameObject FX_SmallBulletHit;
+	void OnCollisionEnter(Collision collision)
+	{
+		try{ //Several ways of doing this. May want to change for performance
+			Entity entityCollision = collision.transform.GetComponent<Entity>();
+			entityCollision.TakeDamage(damage);
+		}
+		catch(NullReferenceException e)
+		{
+			//Do nothing
+		}
+
+		Instantiate(FX_SmallBulletHit,collision.contacts[0].point,Quaternion.identity);
+		Die();
+	}
+
+	void Die()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ UwURevenge/Assets/Projectiles/ProjectileMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour {
+
+	Rigidbody rb;
+	public float speed;
+	public float currentLife;
+	public float maxLife;
+	public string layerToIgnore;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		Physics.IgnoreLayerCollision(LayerMask.NameToLayer(layerToIgnore),LayerMask.NameToLayer(layerToIgnore));
+		Move();
+	}
+	
+	void Update () {
+		currentLife += Time.deltaTime;
+		if(currentLife >= maxLife)
+		{
+			Die();
+		}
+	}
+
+	void FixedUpdate()
+	{
+		
+	}
+
+	void Move()
+	{
+		//transform.Translate(transform.InverseTransformDirection(transform.forward)*speed*Time.deltaTime);
+		rb.AddForce(transform.forward*speed, ForceMode.Impulse);
+	}
+	void Die()
+	{
+		Destroy(gameObject);
+	}
+}
