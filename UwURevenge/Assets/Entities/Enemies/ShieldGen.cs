e15f820396d7562cecf83b14156ddf102a1e3bb8
Kyle Fricilone <kfricilone@gmail.com>
2019-03-09 02:29:49 -0600

    Add initial shield manager/generator

---
+++ UwURevenge/Assets/Entities/Enemies/ShieldGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShieldGen : Entity
+{
+    public ShieldArr parentArray;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentArray.shields.Add(gameObject);
+        Debug.Log("Added");
+    }
+
+    
+    public override void Die()
+    {
+        Debug.Log(parentArray.shields.Count);
+        parentArray.OnDeath(gameObject);
+        Debug.Log(parentArray.shields.Count);
+        Destroy(gameObject);
+        
+        //Spawn Broken Shield Gen
+        if(deathEffect != null)
+        {
+            Instantiate(deathEffect,transform.position,Quaternion.identity);
+        }
+    }
+}
